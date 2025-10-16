bbf0135e65509126e14ba8cd590fc2429c865440
Sophie Akoth <akothsophie9023.gmail.com>
2025-10-16 19:45:05 +0300

    corrected

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,61 +1,3 @@
-// const btn = document.querySelector("#newQuote");
-// const quote =document.querySelector("#quoteDisplay");
-// const category = document.querySelector(".category");
-// const quoteInput = document.querySelector("#newQuoteText");
-// const categoryInput = document.querySelector("#newQuoteCategory");
-
-// const quotes =[{
-//    text: "Start where you are. Use what you have. Do what you can.",
-//     category:"Motivation"},
-// {text:"Success is not final, failure is not fatal: It is the courage to continue that counts.",
-//     category:"Resilience"
-// },
-// {text:"Do not wait for the perfect moment. Take the moment and make it perfect.",
-//     category:"Action"
-// },
-// {text:"The quieter you become, the more you can hear.",
-//     category:"Mindfulness"
-// },
-// {text:"Creativity is intelligence having fun",
-//     category:"Creativity"},
-// {text:"Design is not just what it looks like and feels like. Design is how it works",
-//     category:"Design"},
-// {text:"Art enables us to find ourselves and lose ourselves at the same time",
-//     category:"Art"},
-// {text:"A smooth sea never made a skilled sailor",
-//     category:"Growth"}
-
-// ];
-// btn.addEventListener('click',function(){
-//     let random = Math.floor(Math.random()*quotes.length);
-
-//    quote.innerText = quotes[random].text;
-//    category.innerText = quotes[random].category;
-
-
-// })
-
-
-// Adding quotes
-// const btn1 = document.querySelector("btn1");
-// btn1.addEventListener("click")
-
-// function addQuote(){
-//      const newText=quoteInput.value.trim();
-//      const newCategory = categoryInput.value.trim()
-
-//      if(newText){
-//         quotes.push({text:newText ,category:newCategory});
-//         quoteInput.value = "";
-//         categoryInput.value = "";
-        
-//      }else{
-//        alert ("Please enter a quote ")};
-
-// }
-
-
-
 
 //  Array of quote objects
 const quotes = [
@@ -75,8 +17,24 @@ const quotes = [
   function showRandomQuote() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const selected = quotes[randomIndex];
-    quoteDisplay.innerHTML = `"${selected.text}"`;
-    categoryDisplay.innerHTML = `— ${selected.category}`;
+
+    // Clear previous content
+    quoteDisplay.innerHTML = "";
+    categoryDisplay.innerHTML = "";
+
+    //create new elements
+
+    const quoteText =document.createElement("p");
+    quoteText.textContent = `"${selected.text}"`;
+
+    const categoryText = document.createElement("span");
+    categoryText.textContent = `-${selected.category}`;
+
+    // Append to containers
+
+    quoteDisplay.appendChild(quoteText);
+    categoryDisplay.appendChild(categoryText);
+
   }
   
   // Function to create and handle the add-quote form
