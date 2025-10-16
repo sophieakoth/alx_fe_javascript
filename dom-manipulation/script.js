5cd9f437e59b620c91b45a1483121eed5e10c94c
Sophie Akoth <akothsophie9023.gmail.com>
2025-10-16 21:03:56 +0300

    Added code for export and import

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -104,6 +104,24 @@ function importFromJsonFile(event) {
 }
 
 
+// Export Quotes Button
+function exportToJsonFile() {
+  const dataStr = JSON.stringify(quotes, null, 2);
+  const blob = new Blob([dataStr], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "quotes.json";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+
+
+
+
  
   
 
